98e1d8216befb067df4c8c1079a461d219e024a4
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-09-26 20:01:29 +0300

    Assignment completed successfuly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Ken", last_name: "Ben")
+Student.create!(first_name: "Franc", last_name: "Poto")
+Student.create!(first_name: "Truce", last_name: "Harry")
